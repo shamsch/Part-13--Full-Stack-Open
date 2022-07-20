7960bdcbcebd78955ad6870a3751724f8549fd8e
Shamsur <shamsur314@gmail.com>
2022-07-20 23:25:52 +0300

    13.4

--- index.js
+++ index.js
@@ -1,24 +1,93 @@
-require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+require("dotenv").config();
+const { Sequelize, QueryTypes, Model } = require("sequelize");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
+
+class Blog extends Model {}
+
+Blog.init(
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        author: {
+            type: Sequelize.STRING,
+        },
+        urlString: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        likes: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0,
+        },
+    },
+    { sequelize, underscored: true, timestamps: false, modelName: "blog" }
+);
+
+// creates the table if it doesn't exist already
+Blog.sync();
+
+app.get("/api/blogs", async (req, res) => {
+    try {
+        const blogs = await Blog.findAll();
+        res.json(blogs);
+    } catch (err) {
+        console.log(err);
     }
-  },
-})
+});
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    const notes = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    console.log(notes)
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
+app.post("/api/blogs", async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body);
+        res.json(blog);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+    try {
+        const blog = await Blog.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if(!blog){
+            res.status(404).send("Blog not found");
+        }
+        else{
+            res.json("blog deleted");
+        }
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error deleting blog");
+    }
 }
+);
 
-main()
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
