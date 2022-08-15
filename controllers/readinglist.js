e76bb3c45c618c74244b852e5dd36293c365fbb7
Shamsur <shamsur314@gmail.com>
2022-08-15 12:39:16 +0300

    13.22

--- controllers/readinglist.js
+++ controllers/readinglist.js
@@ -1,11 +1,33 @@
 const router = require("express").Router();
 
+const tokenExtractor = require("../middlewares/tokenExtractor");
 const {ReadingList} = require("../models/index");
 
 router.post("/", async (req, res) => {
-    console.log(ReadingList);
     const newReadingList = await ReadingList.create({...req.body, read: false});
     res.json(newReadingList);
 });
+
+router.put("/:id", tokenExtractor ,async (req, res) => {
+    if (!req.decodedToken) {
+		throw new Error("Unauthorized");
+	}
+    const readingList = await ReadingList.findOne({
+        where: {
+            id: req.params.id,
+            userId: req.decodedToken.id,
+        },
+    });
+
+    if (!readingList) {
+        res.status(404).send("ReadingList not found");
+    } else {
+        readingList.read = req.body.read;
+        await readingList.save();
+        res.json({ read: readingList.read });
+    }
+});
+
+
 
 module.exports = router;
